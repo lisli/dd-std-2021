7ee14d81f6b1cb1792e6008762f039415a8a348f
Lisa Yoder <lyoder@lyoder-macbook.local>
2020-11-05 10:07:40 -0500

    mobile and most desktop styles

--- inc/header.php
+++ inc/header.php
@@ -4,8 +4,8 @@
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://use.typekit.net/zkd8rtd.css">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.typekit.net/vsu5xxr.css">
+        <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> -->
         <link href="dist/css/styles.css" rel="stylesheet">
     </head>
     <body>
--- index.php
+++ index.php
@@ -3,41 +3,54 @@
 ?>
 
 <div class="container">
-    <header class="header__container">
-        <div class="header">
-            <div class="header__inner">
-                <picture>
-                    <!--[if IE 9]><video style="display: none;"><![endif]-->
-                    <source srcset="dist/images/horseshoe-icon.png" media="(max-width: 767px)">
-                    <!--[if IE 9]></video><![endif]-->
-                    <img class="header__icon" src="dist/images/horseshoe-icon-desktop.png"/>
-                </picture>
-                <!-- <img class="header__icon" src="dist/images/horseshoe-icon.png"/> -->
-                <div class="header__text">
-                    <div class="header__pre">The Clark Foundation's 8<sup class="header__sup">th</sup> Annual</div>
-                    <h1 class="header__title">Derby<br/>Day</h1>
-                    <div class="header__date">May 2, 2020</div>
+    <header class="header">
+        <div class="header__date">
+            <div>05</div>
+            <div>01</div>
+            <div>21</div>
+        </div>
+        <div class="header__text">
+            <div class="header--small">The Clark Foundation's 9th Annual</div>
+            <div class="header--large">Derby Day</div>
+        </div>
+    </header>
+    <div class="main">
+        <div class="info">
+            <!-- <div class="info__top"> -->
+                <div class="info__date">
+                    <div class="info__date--small">Save the Date</div>
+                    May 1, 2021
+                </div>
+                <div class="info__time">
+                    4:30 PM
+                </div>
+            <!-- </div> -->
+            <div class="info__location">
+                <div class="info__flag">
+                    New Location!
+                </div>
+                The Farm at Eagles Ridge
+                <div class="info__location--small">
+                    465 Long Lane, Lancaster, PA 17603
                 </div>
             </div>
         </div>
-        <div class="subheader">
-            <div class="subheader__time">4:30 pm at the</div>
-            <div class="subheader__location">Lancaster Country Club</div>
+        <div class="text__container">
+            <div class="text">     
+                <p>Excitement is building for the Ninth Annual Derby Day party, held at The Farm at Eagles Ridge.</p>
+                <p>The Farm’s breathtaking views of the pristine countryside of historic Lancaster County provide a beautiful location to celebrate spring.</p>
+                <p>In the tradition of Churchill Downs, enjoy the next best thing to being at the track on race day! Featuring a traditional Derby menu, mint juleps, a ladies and men’s hat contest, wagering for prizes, entertainment, and a live showing of the 147th Kentucky Derby. Pony up and come on out!</p>
+                <p class="text--bold">Check back in February for more details and to purchase your tickets.</p>
+            </div>
         </div>
-    </header>
-    <div class="text__container">
-        <div class="text">     
-            <p>Excitement is building for the Eighth Annual Derby Day party, held at the scenic Lancaster Country Club.</p>
-            <p>The Club’s panoramic view of the lush greens will provide a beautiful location to celebrate the coming of spring and the Run for the Roses.</p>
-            <p>In the tradition of Churchill Downs, enjoy the next best thing to being at the track on race day! Featuring a traditional Derby menu, mint juleps, a ladies' and men’s hat contest, wagering for prizes, entertainment, and a live showing of the 145th Kentucky Derby. Pony up and come on out!</p>
-            <p class="text--bold">Check back in February for more details and to purchase your tickets.</p>
-        </div>
+    </div>
+    <div class="charity__header">
+        All Proceeds Benefit
     </div>
 </div>
 <div class="charity">
-    <div class="charity__text--sm">Last year we raised over</div>
-    <div class="charity__text--lg">1.1 Million for charity</div>
-    <p class="charity__text">All proceeds benefit the Conestoga Valley Education Foundation, Schreiber Center for Pediatric Development, Manheim Township Education Foundation, and Anchorage Breakfast Program.</p>
+   <p>Anchor Lancaster, Conestoga Valley Education Foundation, Lancaster County Conservancy, Manheim Township Education Foundation, Pequea Valley Education Foundation,
+Schreiber Center for Pediatric Development, and Warwick Education Foundation</p>
 </div>
 
 <?
